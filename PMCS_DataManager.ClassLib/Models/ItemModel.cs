bbc3fe16782f6a017689d274e7f4b10f04001373
Marco Preciado <mac46005@gmail.com>
2021-11-24 22:29:30 -0600

    Adding models to DataManager.ClassLib

---
+++ PMCS_DataManager.ClassLib/Models/ItemModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMCS_DataManager.ClassLib.Models
+{
+    public class ItemModel
+    {
+        public int ItemNumber { get; set; }
+        public int TM_Id { get; set; }
+        public string Interval { get; set; }
+        public string Location { get; set; }
+        public string Procedure { get; set; }
+        public string NotMissionCapIf { get; set; }
+    }
+}
---
+++ PMCS_DataManager.ClassLib/Models/TM_PMCSProcedure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMCS_DataManager.ClassLib.Models
+{
+    public class TM_PMCSProcedure
+    {
+        public int Id { get; set; }
+        public string TM_Id { get; set; }
+        public List<ItemModel> Items { get; set; }
+    }
+}
